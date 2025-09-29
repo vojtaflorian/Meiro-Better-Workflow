f68a1bcd911071173fbb3c3ff03f10419b6f3410
Vojta Florian <79914330+vojtaflorian@users.noreply.github.com>
2025-09-29 21:21:41 +0200

    Update meiro-improvements.user.js

--- meiro-improvements.user.js
+++ meiro-improvements.user.js
@@ -5,8 +5,8 @@
 // @author       Vojta Florian
 // @match        *.meiro.io/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=meiro.io
-// @downloadURL  https://raw.githubusercontent.com/vojtaflorian/Meiro-Better-Workflow/refs/heads/main/meiro-improvements.user.js
-// @updateURL    https://raw.githubusercontent.com/vojtaflorian/Meiro-Better-Workflow/refs/heads/main/meiro-improvements.user.js
+// @downloadURL  https://raw.githubusercontent.com/vojtaflorian/Meiro-Improvements/refs/heads/main/meiro-improvements.user.js
+// @updateURL    https://raw.githubusercontent.com/vojtaflorian/Meiro-Improvements/refs/heads/main/meiro-improvements.user.js
 // @grant        none
 // ==/UserScript==
 
